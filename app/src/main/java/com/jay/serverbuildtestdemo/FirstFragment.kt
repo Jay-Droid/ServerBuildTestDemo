0127f62e5b8d7de497e00930e07828e12f70a9ab
王学杰 <xuejie.wang@cityio.cn>
2020-03-05 15:39:28 +0800

    add test code

---
+++ app/src/main/java/com/jay/serverbuildtestdemo/FirstFragment.kt
@@ -0,0 +1,35 @@
+package com.jay.serverbuildtestdemo
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_first.*
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class FirstFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.button_first).setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+        textview_first.append("\nAPPLICATION_ID:\n${BuildConfig.APPLICATION_ID}")
+        textview_first.append("\nBUILD_TYPE:\n${BuildConfig.BUILD_TYPE}")
+        textview_first.append("\nVERSION_NAME:\n${BuildConfig.VERSION_NAME}")
+    }
+}
---
+++ app/src/main/java/com/jay/serverbuildtestdemo/SecondFragment.kt
@@ -0,0 +1,34 @@
+package com.jay.serverbuildtestdemo
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_second.*
+
+/**
+ * A simple [Fragment] subclass as the second destination in the navigation.
+ */
+class SecondFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_second, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        textview_second.append("\nAPPLICATION_ID:\n${BuildConfig.APPLICATION_ID}")
+        textview_second.append("\nBUILD_TYPE:\n${BuildConfig.BUILD_TYPE}")
+        textview_second.append("\nVERSION_NAME:\n${BuildConfig.VERSION_NAME}")
+        view.findViewById<Button>(R.id.button_second).setOnClickListener {
+            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        }
+    }
+}
